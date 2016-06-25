1720b1a1a6e6b380009334e202abada797f2eda1
Tihon <com.tihon@mail.ru>
2016-06-25 18:44:00 +0300

    add get_priv_dir

--- src/su_utils.erl
+++ src/su_utils.erl
@@ -9,9 +9,20 @@
 -author("tihon").
 
 %% API
--export([to_lower/1]).
+-export([to_lower/1, get_priv_dir/1]).
 
 -spec to_lower(binary() | string()) -> binary() | string().
 to_lower(Bin) when is_binary(Bin) ->
   list_to_binary(to_lower(binary_to_list(Bin)));
 to_lower(List) -> string:to_lower(List).
+
+-spec get_priv_dir(atom()) -> string().
+get_priv_dir(App) ->
+  Path = case application:get_key(App, vsn) of
+           {ok, VSNString} ->
+             "./lib/" ++ atom_to_list(App) ++ "-" ++ VSNString;
+           undefined ->
+             {ok, Dir} = file:get_cwd(),
+             Dir
+         end,
+  Path ++ "/priv/".
